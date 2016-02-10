025b3eb661eac72d510cd377b054e1d12dfb501c
Sebastian Norlin <sebastian.norlin@ericsson.com>
2016-02-10 17:18:08 +0100

    complete R script but need cat like concatination function that puts it into a variable, Paste gives some duplicates, maybe unlist or similar will work.

--- SnapToLp/txtToLp.R
+++ SnapToLp/txtToLp.R
@@ -8,11 +8,15 @@ txtToLp <- function(files){
     for( i in 2:length(edgesets)){
       nodes = intersect(nodes,unlist(edgesets[[i]]))
     }
+  }else{
+    nodes = unique(nodes)
   }
   maxN = max(nodes)
   present = vector(length = maxN)
   present[nodes] = TRUE #sets all nodes that have "survived" the intersets to true
-  edgetexts = vector(length = length(edgesets)) #vector that saves the strings
+  edgeRestrictions = vector(length = length(edgesets)) #vector that saves the strings
+  edgeGreaterThan0 = vector(length = length(edgesets))
+  edgesums = vector(length = length(edgesets))
   for( f in 1:length(files)){
     seekedEdges = vector(length = length(edgesets[[f]][,1])) #true false vector if we want that edge
     numberOfEdges = 0 #number of edges that we want
@@ -22,9 +26,17 @@ txtToLp <- function(files){
         numberOfEdges = numberOfEdges + 1
       }
     }
-   edgetexts[f] = paste("x",f,"_",1:numberOfEdges,"<=","y_",unlist(edgesets[[f]][seekedEdges,]),sep = "",collapse = "\n")
-   
+    #xc_ij < yi yj
+    edgeRestrictions[f] = paste("x",f,"_",1:numberOfEdges,"-y_",
+                         unlist(edgesets[[f]][seekedEdges,]),"<=0",sep = "",collapse = "\n")
+    #xc_ij >=0
+    edgeGreaterThan0[f] = paste("x",f,"_",1:numberOfEdges,">=0",sep = "",collapse = "\n")
+    edgesums[f] = paste(paste("x",f,"_",1:numberOfEdges,sep = "",collapse = "+"),"-t>=0")
   }
-  nodetexts = paste("y_",nodes,">=0",sep = "",collapse = "\n") #all the constraints for the nodes
-  return(c(edgetexts,nodetexts))
+  #
+  nodeGreaterThan0 = paste("y_",nodes,">=0",sep = "",collapse = "\n") #all the constraints for the nodes
+  nodesum = paste(paste("y_",nodes,sep = "",collapse = "+"),"<=1")
+  #return(paste("Maximiz t \nSubject to:\n",edgesums,"\n",nodesum,"\n",edgeRestrictions,"\n",
+  #           nodeGreaterThan0,"\n",edgeGreaterThan0,"\nend"))
+  return (0)
 }
